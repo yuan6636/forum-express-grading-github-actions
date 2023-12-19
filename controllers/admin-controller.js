c44853b3cbc6691a1c24e6585b351d6663913a79
yuan6636 <aleniverson777@gmail.com>
2023-12-19 11:22:29 +0800

    feat: add user permission management & user list
    Squashed commits:
    
    [2b6466f] add model table name

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { localFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -6,7 +6,7 @@ const adminController = {
     Restaurant.findAll({
       raw: true
     })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .then(restaurants => res.render('admin/restaurants', { restaurants, isRestaurantActive: true }))
       .catch(err => next(err))
   },
   createRestaurant: (req, res) => {
@@ -91,6 +91,32 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users, isUserActive: true }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't found!")
+
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+
+        user.isAdmin = !user.isAdmin
+        return user.update({ isAdmin: user.isAdmin })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 
