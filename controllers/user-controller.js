143d450b8a77e1fe23884857ff893e89664cd4bd
yuan6636 <aleniverson777@gmail.com>
2023-12-26 17:13:35 +0800

    feat: add favorite/unfavorite button at index page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User, Restaurant, Comment } = db
-
+const { User, Restaurant, Comment, Favorite } = db
 const { localFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -65,7 +64,6 @@ const userController = {
       .catch(err => next(err))
   },
   putUser: (req, res, next) => {
-    const { name } = req.body
     if (Number(req.params.id) !== Number(req.user.id)) {
       res.redirect(`/users/${req.params.id}`)
     }
@@ -79,7 +77,7 @@ const userController = {
         if (!user) throw new Error("User didn't exist!")
 
         return user.update({
-          name,
+          name: req.body.name,
           image: filePath || user.image
         })
       })
@@ -87,6 +85,44 @@ const userController = {
         req.flash('success_messages', '使用者資料編輯成功')
         res.redirect(`/users/${req.params.id}`)
       })
+      .catch(err => next(err))
+  },
+  addFavorite: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Favorite.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId
+        }
+      })
+    ])
+      .then(([restaurant, favorite]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        if (favorite) throw new Error('You have favorited this restaurant!')
+
+        return Favorite.create({
+          userId: req.user.id,
+          restaurantId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeFavorite: (req, res, next) => {
+    return Favorite.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId
+      }
+    })
+      .then(favorite => {
+        if (!favorite) throw new Error("You haven't favorited this restaurant")
+
+        return favorite.destroy()
+      })
+      .then(() => res.redirect('back'))
       .catch(err => next(err))
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ const commentController = require('../controllers/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 const { generalErrorHandler } = require('../middleware/error-handler')
-
 const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
@@ -36,6 +35,9 @@ router.post('/comments', authenticated, commentController.postComment)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
